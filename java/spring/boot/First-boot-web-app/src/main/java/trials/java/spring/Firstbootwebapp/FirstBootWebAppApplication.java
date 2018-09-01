a30e566bfcbe7aaeb1db6977ece41ee8c23a5360
Ahmed Dalatony <ahmed.dalatony@gmail.com>
2018-09-01 14:48:59 +0200

    spring boot trials-1

---
+++ java/spring/boot/First-boot-web-app/src/main/java/trials/java/spring/Firstbootwebapp/FirstBootWebAppApplication.java
@@ -0,0 +1,12 @@
+package trials.java.spring.Firstbootwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBootWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstBootWebAppApplication.class, args);
+	}
+}
---
+++ java/spring/boot/First-boot-web-app/src/test/java/trials/java/spring/Firstbootwebapp/FirstBootWebAppApplicationTests.java
@@ -0,0 +1,16 @@
+package trials.java.spring.Firstbootwebapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstBootWebAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
